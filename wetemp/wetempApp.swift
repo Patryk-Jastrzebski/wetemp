1cefb7a88a7c7e268321b4178e27a01be0a35f7a
Patryk-Jastrzebski <patryk.jastrzebski@iteo.com>
2022-11-01 19:36:20 +0100

    Initial Commit

---
+++ wetemp/wetempApp.swift
@@ -0,0 +1,17 @@
+//
+//  wetempApp.swift
+//  wetemp
+//
+//  Created by Patryk Jastrzębski on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct wetempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
