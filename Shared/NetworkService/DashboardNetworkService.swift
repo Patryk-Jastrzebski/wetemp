d492d1a13def4ffe101c426d8cd02c23a5cd79b2
Patryk Jastrzebski <61121732+Patryk-Jastrzebski@users.noreply.github.com>
2023-01-02 23:49:55 +0100

    [FEATURE] Added settings (#10)

--- Shared/NetworkService/DashboardNetworkService.swift
+++ Shared/NetworkService/DashboardNetworkService.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol DashboardNetworkService {
     func fetchTemperatureData() async throws -> Temperature
+    func fetchHistorical() async throws -> [Historical]
 }
 
 final class DashboardNetworkServiceImpl: DashboardNetworkService {
@@ -23,7 +24,7 @@ final class DashboardNetworkServiceImpl: DashboardNetworkService {
     func fetchTemperatureData() async throws -> Temperature {
         let parameters: Parameters = [
             "station": 1,
-            "t_unit": "f",
+            "t_unit": "c",
             "p_unit": "p"
         ]
         let url = BaseUrl(featurePath: .recent)
@@ -32,6 +33,24 @@ final class DashboardNetworkServiceImpl: DashboardNetworkService {
                                   parameters: parameters)
         let requestResponse: Temperature = try await httpClient.perform(httpRequest: request)
         return requestResponse
-        
+    }
+    
+//    func fetchPredictions() async throws -> Temperature {
+//
+//    }
+    func fetchHistorical() async throws -> [Historical] {
+        let parameters: Parameters = [
+            "station": 1,
+            "t_unit": "c",
+            "p_unit": "p",
+            "n": 30
+        ]
+        let url = BaseUrl(featurePath: .historical)
+        let request = HttpRequest(url: url,
+                                  method: .get,
+                                  parameters: parameters)
+        let requestResponse: [Historical] = try await httpClient.perform(httpRequest: request)
+        print(requestResponse)
+        return requestResponse
     }
 }
